5584ff0e432193cd7d2bbdc64f29fb8ad32cf6d0
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2025-12-08 17:49:26 -0300

    projeto inicial instalado

---
+++ src/test/java/com/javadevweek/gestao_custos/GestaoCustosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javadevweek.gestao_custos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoCustosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
